a2320f39b6f4a356586791698aa3f13afa2d5809
snehaashoka2310 <snehaashoka2310@gmail.com>
2026-02-27 15:41:46 +0530

    Add greeting for Maven in Sample.java

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ class Sample{
   public static void main(String[] args){
     System.out.println("Hello DevOps");
         System.out.println("Hello Jenkins");
-
+          System.out.println("Hello Maven");
   }
 }
